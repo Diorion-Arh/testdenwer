c40f2ab57183bd69b6573aebfd13f18db2f1ac98
Diorion-Arh <65719938+Diorion-Arh@users.noreply.github.com>
2020-09-18 12:35:29 +0300

    1

---
+++ product.php
@@ -0,0 +1,9 @@
+<?php
+require('products_db.php');
+require('components/header.php');
+
+$title = get_product_title(1);
+// Объявите переменную здесь
+
+require('components/product_info.php');
+require('components/footer.php');
